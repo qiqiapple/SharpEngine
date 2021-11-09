48e43895782ce56d051cbcfaaac55120f7de1e48
qiqiapple <qiqiapple@gmail.com>
2021-11-09 15:13:09 +0100

    Clean Up the Code using Methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using GLFW;
 using static OpenGL.Gl;
 
@@ -8,7 +9,62 @@ namespace SharpEngine
     {
         static void Main(string[] args)
         {
-            // initialize and configure
+            var window = CreateWindow();
+            LoadTriangleIntoBuffer();
+            CreateShaderProgram();
+
+            // engine rendering loop
+            while (!Glfw.WindowShouldClose(window))
+            {
+                Glfw.PollEvents();
+                glDrawArrays(GL_TRIANGLES, 0,3);
+                glFlush();
+            }
+        }
+
+        private static void CreateShaderProgram()
+        {
+            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+            glShaderSource(vertexShader, File.ReadAllText("shaders/red-triangle.vert"));
+            glCompileShader(vertexShader);
+
+            // create fragment shader
+            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+            glShaderSource(fragmentShader, File.ReadAllText("shaders/red-triangle.frag"));
+            glCompileShader(fragmentShader);
+
+            // create shader program - rendering pipeline
+            var program = glCreateProgram();
+            glAttachShader(program, vertexShader);
+            glAttachShader(program, fragmentShader);
+            glLinkProgram(program);
+            glUseProgram(program);
+        }
+
+        private static unsafe void LoadTriangleIntoBuffer()
+        {
+            float[] vertices = new float[]
+            {
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0f, 0.5f, 0f
+            };
+
+            // load the vertices into a buffer
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            fixed (float* vertex = &vertices[0])
+            {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
+            }
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * sizeof(float), NULL);
+            glEnableVertexAttribArray(0);
+        }
+
+        private static Window CreateWindow()
+        {
             Glfw.Init();
             Glfw.WindowHint(Hint.ClientApi, ClientApi.OpenGL);
             Glfw.WindowHint(Hint.ContextVersionMajor, 3);
@@ -17,76 +73,12 @@ namespace SharpEngine
             Glfw.WindowHint(Hint.OpenglProfile, Profile.Core);
             Glfw.WindowHint(Hint.OpenglForwardCompatible, Constants.True);
             Glfw.WindowHint(Hint.Doublebuffer, Constants.False);
-            
+
             // create and launch a window
             var window = Glfw.CreateWindow(1024, 768, "SharpEngine", Monitor.None, Window.None);
             Glfw.MakeContextCurrent(window);
             Import(Glfw.GetProcAddress);
-            float[] vertices = new float[]
-            {
-                -0.5f, -0.5f, 0f,
-                0.5f, -0.5f, 0f,
-                0f, 0.5f, 0f
-            };
-            
-            // load the vertices into a buffer
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER,vertexBuffer);
-            unsafe
-            {
-                fixed (float* vertex = &vertices[0])
-                {
-                    glBufferData(GL_ARRAY_BUFFER,sizeof(float) * vertices.Length, vertex, GL_STATIC_DRAW);
-                }
-                glVertexAttribPointer(0,3,GL_FLOAT,false,3*sizeof(float),NULL);
-            }
-            glEnableVertexAttribArray(0);
-            string vertexShaderSource = @"
-#version 330 core
-in vec3 pos;
-void main()
-{
-    gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-}
-";
-
-            string fragmentShaderSource = @"
-#version 330 core
-out vec4 result;
-void main()
-{
-    result = vec4(1, 0, 0, 1);
-}
-";
-
-            // create vertex shader
-            var vertexShader = glCreateShader(GL_VERTEX_SHADER);
-            glShaderSource(vertexArray, vertexShaderSource);
-            glCompileShader(vertexShader);
-            
-            // create fragment shader
-            var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-            glShaderSource(fragmentShader, fragmentShaderSource);
-            glCompileShader(fragmentShader);
-            
-            // create shader program - rendering pipeline
-            var program = glCreateProgram();
-            glAttachShader(program, vertexShader);
-            glAttachShader(program, fragmentShader);
-            glLinkProgram(program);
-            glUseProgram(program);
-            
-            // engine rendering loop
-            while (!Glfw.WindowShouldClose(window))
-            {
-                Glfw.PollEvents();
-                glDrawArrays(GL_TRIANGLES, 0,3);
-                //Glfw.SwapBuffers(window);
-                glFlush();
-            }
-
+            return window;
         }
     }
 }

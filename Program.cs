61407df7baa7a68d347d062885978faa5d129b95
qiqiapple <qiqiapple@gmail.com>
2021-11-11 19:59:05 +0100

    Move Rotate, Bounce and LoadTriangleIntoBuffer to Triangle class

--- Program.cs
+++ Program.cs
@@ -30,16 +30,11 @@ namespace SharpEngine
             }
         );
 
-        private static Vector velocity = new Vector(0.003f, 0.002f);
-        private static Vector velocity2 = new Vector(0.003f, 0.002f);
         private static float multiplier = 0.999f;
-        private static Vector center;
-        private static Vector center2;
 
         static void Main(string[] args)
         {
             var window = CreateWindow();
-            LoadTriangleIntoBuffer();
             CreateShaderProgram();
             
             // engine rendering loop
@@ -54,54 +49,17 @@ namespace SharpEngine
 
         private static void Play()
         {
-            center = triangle.GetCenter();
-            center2 = triangle2.GetCenter();
-            ChangeDirection();
-            triangle.Move(velocity);
-            triangle2.Move(velocity2);
-            Rotate();
+            //ChangeDirection();
+            triangle.Bounce();
+            triangle2.Bounce();
+            triangle.Move();
+            triangle2.Move();
+            triangle.Rotate();
+            triangle2.Rotate();
             if (triangle.CurrentScale >= 1f) multiplier = 0.999f;
             if (triangle.CurrentScale <= 0.5f) multiplier = 1.001f;
             triangle.Scale(multiplier);
             triangle2.Scale(multiplier);
-        }
-
-        private static void ChangeDirection()
-        {
-            if (triangle.GetMaxBounds().x >= 1 && velocity.x>0 || triangle.GetMinBounds().x<=-1 && velocity.x<0)
-            {
-                velocity.x *= -1;
-            }
-            if (triangle.GetMaxBounds().y >= 1 && velocity.y>0 || triangle.GetMinBounds().y<=-1 && velocity.y<0)
-            {
-                velocity.y *= -1;
-            }
-            
-            if (triangle2.GetMaxBounds().x >= 1 && velocity2.x>0 || triangle2.GetMinBounds().x<=-1 && velocity2.x<0)
-            {
-                velocity2.x *= -1;
-            }
-            if (triangle2.GetMaxBounds().y >= 1 && velocity2.y>0 || triangle2.GetMinBounds().y<=-1 && velocity2.y<0)
-            {
-                velocity2.y *= -1;
-            }
-        }
-        
-        private static void Rotate()
-        {
-            float angle = 0.003f;
-            Vector rotation1 = new Vector(MathF.Cos(angle), MathF.Sin(angle));
-            Vector rotation2 = new Vector(-MathF.Sin(angle), MathF.Cos(angle));
-            for (int i = 0; i < triangle.vertices.Length; i++)
-            {
-                Vector vector2 = triangle.vertices[i].position - center;
-                triangle.vertices[i].position = new Vector(rotation1 * vector2, rotation2 * vector2) + center;
-            }
-            for (int i = 0; i < triangle2.vertices.Length; i++)
-            {
-                Vector vector2 = triangle2.vertices[i].position - center2;
-                triangle2.vertices[i].position = new Vector(rotation1 * vector2, rotation2 * vector2) + center2;
-            }
         }
 
         private static void Render(Window window)
@@ -134,20 +92,6 @@ namespace SharpEngine
             glAttachShader(program, fragmentShader);
             glLinkProgram(program);
             glUseProgram(program);
-        }
-
-        private static unsafe void LoadTriangleIntoBuffer()
-        {
-            // load the vertices into a buffer
-            var vertexArray = glGenVertexArray();
-            var vertexBuffer = glGenBuffer();
-            glBindVertexArray(vertexArray);
-            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-            //glVertexAttribPointer(0, vertexSize, GL_FLOAT, false, vertexSize * sizeof(float), NULL);
-            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), NULL);
-            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), (void*)sizeof(Vector));
-            glEnableVertexAttribArray(0);
-            glEnableVertexAttribArray(1);
         }
 
         private static Window CreateWindow()
--- Triangle.cs
+++ Triangle.cs
@@ -1,16 +1,19 @@
+using System;
 using OpenGL;
-
+using static OpenGL.Gl;
 namespace SharpEngine
 {
     public class Triangle
     {
         public Vertex[] vertices;
+        public Vector velocity;
         public float CurrentScale { get; private set; }
-
         public Triangle(Vertex[] vertices)
         {
             this.vertices = vertices;
             this.CurrentScale = 1f;
+            this.velocity = new Vector(0.003f, 0.002f);
+            LoadTriangleIntoBuffer();
         }
 
         public Vector GetMinBounds()
@@ -48,12 +51,45 @@ namespace SharpEngine
 
             this.CurrentScale *= multiplier;
         }
+        
+        public void Rotate()
+        {
+            float angle = 0.003f;
+            var center = GetCenter();
+            Vector rotation1 = new Vector(MathF.Cos(angle), MathF.Sin(angle));
+            Vector rotation2 = new Vector(-MathF.Sin(angle), MathF.Cos(angle));
+            for (int i = 0; i < this.vertices.Length; i++)
+            {
+                Vector vector2 = this.vertices[i].position - center;
+                this.vertices[i].position = new Vector(rotation1 * vector2, rotation2 * vector2) + center;
+            }
+        }
+        
+        public void Move()
+        {
+            for (int i = 0; i < this.vertices.Length; i++)
+            {
+                this.vertices[i].position += this.velocity;
+            }
+        }
 
         public void Move(Vector direction)
         {
             for (int i = 0; i < this.vertices.Length; i++)
             {
                 this.vertices[i].position += direction;
+            }
+        }
+
+        public void Bounce()
+        {
+            if (this.GetMaxBounds().x >= 1 && this.velocity.x>0 || this.GetMinBounds().x<=-1 && this.velocity.x<0)
+            {
+                this.velocity.x *= -1;
+            }
+            if (this.GetMaxBounds().y >= 1 && this.velocity.y>0 || this.GetMinBounds().y<=-1 && this.velocity.y<0)
+            {
+                this.velocity.y *= -1;
             }
         }
 
@@ -64,6 +100,20 @@ namespace SharpEngine
                 Gl.glBufferData(Gl.GL_ARRAY_BUFFER, sizeof(Vertex) * this.vertices.Length, vertex, Gl.GL_STATIC_DRAW);
             }
             Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, this.vertices.Length);
+        }
+        
+        public unsafe void LoadTriangleIntoBuffer()
+        {
+            // load the vertices into a buffer
+            var vertexArray = glGenVertexArray();
+            var vertexBuffer = glGenBuffer();
+            glBindVertexArray(vertexArray);
+            glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+            //glVertexAttribPointer(0, vertexSize, GL_FLOAT, false, vertexSize * sizeof(float), NULL);
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(Vertex), NULL);
+            glVertexAttribPointer(1, 4, GL_FLOAT, false, sizeof(Vertex), (void*)sizeof(Vector));
+            glEnableVertexAttribArray(0);
+            glEnableVertexAttribArray(1);
         }
     }
 }
